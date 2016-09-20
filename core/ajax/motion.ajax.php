f53fec95e8302c7de235062170d20814b61b1317
Halbout Michael <michaelhalbout@gmail.com>
2016-09-20 11:43:06 +0200

    Update motion.ajax.php

--- core/ajax/motion.ajax.php
+++ core/ajax/motion.ajax.php
@@ -1,149 +1,145 @@
 <?php
-try {
-    require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
-    include_file('core', 'authentification', 'php');
-
-    if (!isConnect('admin')) {
-        throw new Exception(__('401 - Accès non autorisé', __FILE__));
-    }
-	if (init('action') == 'SearchCamera') {
-		$EqLogic = eqLogic::byType('camera');
-    /*    if (!is_object($EqLogic)) {
+	try {
+		require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+		include_file('core', 'authentification', 'php');
+		
+		if (!isConnect('admin')) {
+			throw new Exception(__('401 - Accès non autorisé', __FILE__));
+		}
+		if (init('action') == 'SearchCamera') {
+			$EqLogic = eqLogic::byType('camera');
+			/*    if (!is_object($EqLogic)) {
 			// ajax::success(false);
-        }*/
-		$return=array();
-		foreach($EqLogic as $Camera)
+			}*/
+			$return=array();
+			foreach($EqLogic as $Camera)
 			$return[]=array('Nom'=>$Camera->getName(),'Id'=>$Camera->getID());
-		ajax::success($return);
-    }
-	if (init('action') == 'SearchUSBCamera') {
-		$USBCamera=array();
-		foreach (motion::getUsbMapping() as $name => $value) {
+			ajax::success($return);
+		}
+		if (init('action') == 'SearchUSBCamera') {
+			$USBCamera=array();
+			foreach (motion::getUsbMapping() as $name => $value) {
 			$USBCamera[]=array('name'=>$name,'value'=>$value);
 		}
 		ajax::success($USBCamera);
-	}
-	if (init('action') == 'removeRecord') {
-		$result;
-        $file = init('file');
-		$Camera=eqLogic::byId(init('cameraId'));
-		if (is_object($Camera))
-		{
-			$result=motion::removeRecord($file);
-			ajax::success($result);
 		}
-		ajax::success(false);
-    }
-	if (init('action') == 'WidgetHtml') {
-		$MotionCamera=eqLogic::byId(init('cameraId'));
-		if (is_object($MotionCamera))
-		{
-			ajax::success($MotionCamera->toHtml('dashboard'));
-		}
-		ajax::success(false);
-    }
-	if (init('action') == 'RefreshFlux') {
-		$MotionCamera=eqLogic::byId(init('cameraId'));
-		if (is_object($MotionCamera))
-		{
-			ajax::success($MotionCamera->getSnapshot());
-		}
-		ajax::success(false);
-    }
-	if (init('action') == 'getCamera') {
-		if (init('object_id') == '') {
-			$object = object::byId($_SESSION['user']->getOptions('defaultDashboardObject'));
-		} else {
-			$object = object::byId(init('object_id'));
-		}
-		if (!is_object($object)) {
-			$object = object::rootObject();
-		}
-		$return = array();
-		$return['eqLogics'] = array();
-		if (init('object_id') == '') {
-			foreach (object::all() as $object) {
-				foreach ($object->getEqLogic(true, false, 'motion') as $camera) {
-					$return['eqLogics'][] = $camera->toHtml(init('version'));
-				}
+		if (init('action') == 'removeRecord') {
+			$result;
+			$file = init('file');
+			$Camera=eqLogic::byId(init('cameraId'));
+			if (is_object($Camera)){
+				$result=motion::removeRecord($file);
+				ajax::success($result);
 			}
-		} else {
-			foreach ($object->getEqLogic(true, false, 'motion') as $camera) {
-				$return['eqLogics'][] = $camera->toHtml(init('version'));
+			ajax::success(false);
+		}
+		if (init('action') == 'WidgetHtml') {
+			$MotionCamera=eqLogic::byId(init('cameraId'));
+			if (is_object($MotionCamera))
+				ajax::success($MotionCamera->toHtml('dashboard'));
+			ajax::success(false);
+		}
+		if (init('action') == 'RefreshFlux') {
+			$MotionCamera=eqLogic::byId(init('cameraId'));
+			if (is_object($MotionCamera))
+				ajax::success($MotionCamera->getSnapshot());
+		ajax::success(false);
+		}
+		if (init('action') == 'getCamera') {
+			if (init('object_id') == '') {
+				$object = object::byId($_SESSION['user']->getOptions('defaultDashboardObject'));
+			} else {
+				$object = object::byId(init('object_id'));
 			}
-			foreach (object::buildTree($object) as $child) {
-				$cameras = $child->getEqLogic(true, false, 'motion');
-				if (count($cameras) > 0) {
-					foreach ($cameras as $camera) {
+			if (!is_object($object)) {
+				$object = object::rootObject();
+			}
+			$return = array();
+			$return['eqLogics'] = array();
+			if (init('object_id') == '') {
+				foreach (object::all() as $object) {
+					foreach ($object->getEqLogic(true, false, 'motion') as $camera) {
 						$return['eqLogics'][] = $camera->toHtml(init('version'));
 					}
 				}
-			}
-		}
-		ajax::success($return);
-	}
-	if (init('action') == 'getLog') {
-		ajax::success("<pre>".file_get_contents('/etc/motion/motion.log')."</pre>");
-	}
-	if (init('action') == 'removeLog') {
-		exec('sudo rm /etc/motion/motion.log > /dev/null 2>/dev/null &');
-		ajax::success("Suppression faite");
-	}
-	if (init('action') == 'getVideoConvertionStat') {
-		$dir=dirname(__FILE__) . '/../../../../tmp/';
-		if(file_exists($dir.'video.mp4'))
-	 		exec('sudo rm '.$dir.'video.mp4');
-		if(file_exists($dir.'video.ogv'))
-			exec('sudo rm '.$dir.'video.ogv');
-		if(file_exists($dir.'video.webm'))
-			exec('sudo rm '.$dir.'video.webm');
-		shell_exec('sudo ffmpeg -i '.init('src').' -vcodec libx264 '.$dir.'video.mp4 1> '.$dir.'block.txt 2>&1 > /dev/null 2>/dev/null &');
-		//exec('sudo ffmpeg -i '.init('src').' -vcodec libtheora '.$dir.'video.ogv');
-		//exec('sudo ffmpeg -i '.init('src').'  -b 1000k '.$dir.'video.webm');
-		$duration = 0;
-		$time = 0;
-		$progress = 0;
-		$result = array();
-		if(!$log=@fopen($dir.'block.txt',"r")){
-			ajax::error('Impossible d\'ouvrir le fichier : '.$dir.'block.txt');
-		}
-		else {
-			$content=fgets($log);
-			preg_match('/Duration: (.*?), start:/',$content,$matches) ;
-			if(count($matches)>0 ){
-				$rawDuration = $matches[1];
-				// convert rawDuration from 00:00:00.00 to seconds.
-				$ar = array_reverse(explode(":",$rawDuration));
-				$duration = floatval($ar[0]);
-				if ($ar[1]) $duration += intval($ar[1]) * 60;
-				if ($ar[2]) $duration += intval($ar[2]) * 60 * 60;
-				
-				// get the time 
-			
-				preg_match('/time=(.*?) bitrate/g',$content,$matches) ;
-				if(count($matches)>0 ){
-					$rawTime = array_pop($matches);
-					$rawTime = str_replace('time=','',$rawTime);
-					$rawTime = str_replace(' bitrate','',$rawTime);
-					
-					// convert rawTime from 00:00:00.00 to seconds.
-					$ar = array_reverse(explode(":",$rawTime));
-					$time = floatval($ar[0]);
-					if ($ar[1])$time += intval($ar[1]) * 60;
-					if ($ar[2]) $time += intval($ar[2]) * 60 * 60;
-					
-					//calculate the progress
-					$progress = round(($time/$duration) * 100);
+			} else {
+				foreach ($object->getEqLogic(true, false, 'motion') as $camera) {
+					$return['eqLogics'][] = $camera->toHtml(init('version'));
 				}
-				$result['duration'] = $duration;
-				$result['current']  = $time;
-				$result['progress'] = $progress;
+				foreach (object::buildTree($object) as $child) {
+					$cameras = $child->getEqLogic(true, false, 'motion');
+					if (count($cameras) > 0) {
+						foreach ($cameras as $camera) {
+							$return['eqLogics'][] = $camera->toHtml(init('version'));
+						}
+					}
+				}
 			}
-			ajax::success($result);
+			ajax::success($return);
+		}
+		if (init('action') == 'getLog') {
+			ajax::success("<pre>".file_get_contents('/etc/motion/motion.log')."</pre>");
+		}
+		if (init('action') == 'removeLog') {
+			exec('sudo rm /etc/motion/motion.log > /dev/null 2>/dev/null &');
+			ajax::success("Suppression faite");
+		}
+		if (init('action') == 'getVideoConvertionStat') {
+			$dir=dirname(__FILE__) . '/../../../../tmp/';
+			if(file_exists($dir.'video.mp4'))
+				exec('sudo rm '.$dir.'video.mp4');
+			if(file_exists($dir.'video.ogv'))
+				exec('sudo rm '.$dir.'video.ogv');
+			if(file_exists($dir.'video.webm'))
+				exec('sudo rm '.$dir.'video.webm');
+			shell_exec('sudo ffmpeg -i '.init('src').' -vcodec libx264 '.$dir.'video.mp4 1> '.$dir.'block.txt 2>&1 > /dev/null 2>/dev/null &');
+			//exec('sudo ffmpeg -i '.init('src').' -vcodec libtheora '.$dir.'video.ogv');
+			//exec('sudo ffmpeg -i '.init('src').'  -b 1000k '.$dir.'video.webm');
+			$duration = 0;
+			$time = 0;
+			$progress = 0;
+			$result = array();
+			if(!$log=@fopen($dir.'block.txt',"r")){
+				ajax::error('Impossible d\'ouvrir le fichier : '.$dir.'block.txt');
+			}
+			else {
+				$content=fgets($log);
+				preg_match('/Duration: (.*?), start:/',$content,$matches) ;
+				if(count($matches)>0 ){
+					$rawDuration = $matches[1];
+					// convert rawDuration from 00:00:00.00 to seconds.
+					$ar = array_reverse(explode(":",$rawDuration));
+					$duration = floatval($ar[0]);
+					if ($ar[1]) $duration += intval($ar[1]) * 60;
+					if ($ar[2]) $duration += intval($ar[2]) * 60 * 60;
+					
+					// get the time 
+					
+					preg_match('/time=(.*?) bitrate/g',$content,$matches) ;
+					if(count($matches)>0 ){
+						$rawTime = array_pop($matches);
+						$rawTime = str_replace('time=','',$rawTime);
+						$rawTime = str_replace(' bitrate','',$rawTime);
+						
+						// convert rawTime from 00:00:00.00 to seconds.
+						$ar = array_reverse(explode(":",$rawTime));
+						$time = floatval($ar[0]);
+						if ($ar[1])$time += intval($ar[1]) * 60;
+						if ($ar[2]) $time += intval($ar[2]) * 60 * 60;
+						
+						//calculate the progress
+						$progress = round(($time/$duration) * 100);
+					}
+					$result['duration'] = $duration;
+					$result['current']  = $time;
+					$result['progress'] = $progress;
+				}
+				ajax::success($result);
+			}
 		}	
-	throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
-    /*     * *********Catch exeption*************** */
-} catch (Exception $e) {
-    ajax::error(displayExeption($e), $e->getCode());
-}
+		throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
+		/*     * *********Catch exeption*************** */
+	} catch (Exception $e) {
+		ajax::error(displayExeption($e), $e->getCode());
+	}
 ?>
