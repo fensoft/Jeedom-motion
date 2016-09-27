a44259eabeb71a3298d0859b85a0ccb33a00385c
Halbout Michael <michaelhalbout@gmail.com>
2016-09-27 10:44:30 +0200

    Update motion.history.php

--- desktop/modal/motion.history.php
+++ desktop/modal/motion.history.php
@@ -93,7 +93,7 @@ foreach ($files as $date => &$file) {
 		if(typeof $(this).attr('data-filename') != 'undefined'){
 			RemoveFile($(this).attr('data-filename'));
 		}else {
-			alert($(this).html());
+			alert($(this).children('.bt_removeCameraFile').length);
 			$(this).children('.bt_removeCameraFile').each(function() {
 				alert($(this).attr('data-filename'));				
 				if(typeof $(this).attr('data-filename') != 'undefined')
