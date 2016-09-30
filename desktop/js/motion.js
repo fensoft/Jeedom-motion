8fecf702d74b3f229efcf882d131277223519563
Halbout Michael <michaelhalbout@gmail.com>
2016-09-30 13:24:48 +0200

    Update motion.js

--- desktop/js/motion.js
+++ desktop/js/motion.js
@@ -1,5 +1,25 @@
 $("#table_cmd").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
 var areas='';
+//$(function() {
+	var el, newPoint, newPlace, offset;
+	$("input[type='range']").change(function() {
+	el = $(this);
+	width = el.width();
+	newPoint = (el.val() - el.attr("min")) / (el.attr("max") - el.attr("min"));
+	offset = -1.3;)
+	if (newPoint < 0) { newPlace = 0; }
+	else if (newPoint > 1) { newPlace = width; }
+	else { newPlace = width * newPoint + offset; offset -= newPoint; }
+	el
+	.next("output")
+	.css({
+		left: newPlace,
+		marginLeft: offset + "%"
+	})
+	.text(el.val());
+	})
+	.trigger('change');
+//});
 $.ajax({
 	type: "POST",
 	timeout:8000, 
